096feb52175cb1fd798e544b8aa31c1e6b1cb211
Jakub Sztandera <kubuxu@protonmail.ch>
2017-02-01 13:17:54 +0100

    Unsilience gx
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>

--- mk/gx.mk
+++ mk/gx.mk
@@ -1,6 +1,6 @@
 gx-path = gx/ipfs/$(shell gx deps find $(1))/$(1)
 
 gx-deps: bin/gx bin/gx-go $(CHECK_GO)
-	gx install --global >/dev/null 2>&1
+	gx --verbose install --global
 
 DEPS_GO += gx-deps
